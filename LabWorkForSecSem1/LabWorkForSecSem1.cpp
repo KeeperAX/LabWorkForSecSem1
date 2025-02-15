e7a24597f45562f9d2a9ce999547b08fa82fb1bc
Алексей Коврижных <alexkovrizh@gmail.com>
2025-02-15 18:18:43 +0300

    1.1

--- LabWorkForSecSem1/LabWorkForSecSem1.cpp
+++ LabWorkForSecSem1/LabWorkForSecSem1.cpp
@@ -1,111 +1,57 @@
 ﻿#include <iostream>
-#include <list>
 
 using namespace std;
 
-struct Node
-{
+struct Node {
     int val;
     Node* next;
 
     Node(int _val) : val(_val), next(nullptr) {}
 };
 
-struct List
-{
-    Node* first;  //Node* second;
-    List() : first(nullptr){};
+struct Stack {
+    Node* top;  
+    Stack() : top(nullptr) {}
 
     void push_buck(int value) {
         Node* newNode = new Node(value);
-       if (!first) {
-            first = newNode; 
-            return;
-        }
-        Node* temp = first;
-        while (temp->next) {
-            temp = temp->next; 
-        }
-        temp->next = newNode; 
-        //newNode->next = first; 
-        //first = newNode; 
+        newNode->next = top;  
+        top = newNode;       
     }
 
     void print() {
-        Node* temp = first;
-        while (temp) {
-            cout << temp->val << " -> ";
-            temp = temp->next;
-        }
-
+        printHelper(top);
     }
 
-    void pop() {
-        if (!first) {
+    void printHelper(Node* node) {
+        if (node == nullptr) {
             return;
         }
-        Node* temp = first; 
-        first = first->next; 
-        delete temp; 
-    }
-
-
-    
-    void sort() {
-        bool swap;
-        do {
-            swap = false;
-            Node* current = first;
-            Node* nextNode = first->next;
-
-            while (nextNode) {
-                if (current->val > nextNode->val) {
-                    
-                    swap(current->val, nextNode->val);
-                    swap = true; 
-                }
-                current = nextNode;
-                nextNode = nextNode->next;
-            }
-        } while (swap); 
-    }
-
-    void Reverse() {
-        List positiveList; 
-        Node* temp = first;
-
-        while (temp) {
-            if (temp->val > 0) { 
-                positiveList.push_buck(temp->val); 
-            }
-            temp = temp->next;
+        printHelper(node->next);
+        
+        if (node->val > 0) {
+            cout << node->val << " ";
         }
-
-        positiveList.print(); 
     }
 };
 
-int main()
-{
-    List arr;
-    short num = 1;
-    while (true)
-    {
+int main() {
+    setlocale(LC_ALL, "RU");
+    Stack stack;
+    int num;
+    
+    cout << "Введите числа (0 для завершения ввода):" << endl;
+    while (true) {
         cin >> num;
-        if (num != 0)
-        {
-            arr.push_buck(num);
+        if (num == 0) {
+            break;  
         }
-        else { break; }
+        stack.push_buck(num);  
     }
-    cout << "Исходный список: ";
-	arr.print();
-    arr.sort();
-    cout << "\nОтсортированные список: ";
-    arr.Reverse();
 
-   
+    cout << "Положительные числа в обратном порядке: ";
+    stack.print();  
+    cout << endl;
+
     return 0;
 }
-
-
